87938cf783dea0f0454ce619998f3a90b8e93746
Raphaël P. Barazzutti <rbarazzutti@apache.org>
2015-11-08 16:29:07 +0100

    added documentation for hid_send_feature_report

--- src/main/java/ch/fever/jhidapi/jna/HidApiNative.java
+++ src/main/java/ch/fever/jhidapi/jna/HidApiNative.java
@@ -164,6 +164,22 @@ public interface HidApiNative extends Library {
      */
     int hid_set_nonblocking(HidDevice device, int nonblock);
 
+    /**
+     * Send a Feature report to the device.
+     *
+     * Feature reports are sent over the Control endpoint as a Set_Report transfer. The first byte of data[] must
+     * contain the Report ID. For devices which only support a single report, this must be set to 0x0. The remaining
+     * bytes contain the report data. Since the Report ID is mandatory, calls to hid_send_feature_report() will always
+     * contain one more byte than the report contains. For example, if a hid report is 16 bytes long, 17 bytes must be
+     * passed to hid_send_feature_report(): the Report ID (or 0x0, for devices which do not use numbered reports),
+     * followed by the report data (16 bytes). In this example, the length passed in would be 17.
+     *
+     * @param device A device handle returned from hid_open().
+     * @param data   The data to send, including the report number as the first byte.
+     * @param length The length in bytes of the data to send, including the report number.
+     * @return This function returns the actual number of bytes written and -1 on error.
+     */
+    
     int hid_send_feature_report(HidDevice device, FeatureReport data, int length);
 
     /**
